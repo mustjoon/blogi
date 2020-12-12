7c096363520c94de2dbb3574c757968c23054702
= <mustjoon@gmail.com>
2020-12-12 18:07:25 +0200

    fixed linting script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import { Provider } from 'react-redux'
-import store from '../src/store'
+import { Provider } from 'react-redux';
+import store from '../src/store';
 
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps } : {Component: any, pageProps: any} ) {
+function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
   return (
     <Provider store={store}>
-  <Component {...pageProps} />
-  </Provider>
-  )
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
