c7284ee76b0f521b4adc5aa7fd64a9549297f95d
= <mustjoon@gmail.com>
2020-12-12 17:20:23 +0200

    minor eslint changes

--- pages/movie/index.tsx
+++ pages/movie/index.tsx
@@ -1,23 +1,16 @@
-import {useSelector, useDispatch} from 'react-redux'
-import {resetMovies} from 'src/slices/movie'
+import { useSelector, useDispatch } from 'react-redux';
+import { FunctionComponent } from 'react';
+import { resetMovies } from 'src/slices/movie';
 
-export const Movie = () => {
-    const dispatch = useDispatch()
-                     const { entities, totalEntities, error, loading } = useSelector(
-        (state) => state.movie,
+export const Movie: FunctionComponent = () => {
+  const dispatch = useDispatch();
+  const { entities, totalEntities, error, loading } = useSelector((state) => state.movie);
 
-      )
+  const onClick = (): void => {
+    dispatch(resetMovies());
+  };
 
-    const onClick = (): void => {
-        dispatch(resetMovies())
-    }
+  return <div onClick={onClick}>{JSON.stringify(entities)}</div>;
+};
 
-    
-    return (
-        <div onClick={onClick}>
-            {JSON.stringify(entities)}
-        </div>
-    )
-}
-
-export default Movie
+export default Movie;
