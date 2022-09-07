6cc841290bfc93bca70f84b4f93fb81485210af9
joonasmustonen <joonas.mustonen@kaimana.fi>
2022-09-07 11:36:02 +0300

    reverted unneeded change, merge

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,6 @@ export const Layout: FunctionComponent<any> = ({ children }: any) => {
   return (
     <LayoutContainer>
       <Header />
-      X
       <div>{children}</div>
     </LayoutContainer>
   );
