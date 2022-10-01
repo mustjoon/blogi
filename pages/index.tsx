819e46ad2663d8eb1cb92b4904a9134edb95d0a1
joonasmustonen <joonas.mustonen@kaimana.fi>
2022-10-01 11:21:13 +0300

    index page redirect to blogs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
-import Blog from 'pages/blog';
+export async function getServerSideProps({ query }): Promise<any> {
+  return {
+    redirect: {
+      permament: false,
+      destination: '/blog',
+    },
+  };
+}
 
-export default Blog;
+export default function (): null {
+  return null;
+}
