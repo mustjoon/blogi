1265d72bd7811083f3126860d9040d3439a3a7d2
joonasmustonen <joonas.mustonen@kaimana.fi>
2022-12-28 19:01:06 +0200

    added gtm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,8 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import { Html, Head, Main, NextScript } from 'next/document';
+
 export default class MyDocument extends Document {
   static async getInitialProps(
     ctx: any
@@ -32,5 +34,32 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-NX5RQNK');`,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id='GTM-NX5RQNK" height="0" width="0" style="display: none; visibility: hidden;" />`,
+            }}
+          />
+        </body>
+      </Html>
+    );
   }
 }
