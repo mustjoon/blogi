2d5580b158a22bb5596ff6d1c7ba66739e5e1ec5
joonasmustonen <joonas.mustonen@kaimana.fi>
2023-01-26 13:33:08 +0200

    gtm defer

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,6 +39,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <script
+            defer
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
